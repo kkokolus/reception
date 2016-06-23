2c9d9f4aa3bb1930a288e86cd0228852d5899a60
V Borokhov <wborochow@gmail.com>
2016-06-24 00:50:55 +0200

    Dziala dodawanie wizyt, poprawilem byki w wypelnianiu siatki tygodniowej

--- recepcja/Baza/Baza/MainWindow.xaml.cs
+++ recepcja/Baza/Baza/MainWindow.xaml.cs
@@ -33,6 +33,11 @@ namespace Baza
         IQueryable qry;
         IQueryable q3;
         string godzinaWizyty = "";
+        string selectedDate = "";
+        int numerDniaTygodnia = 0;
+        string nazwisko_lekarza = "";
+        int idWybranegoLekarza = 0;
+        int iddyzur;
         public MainWindow()
         {
             InitializeComponent();
@@ -240,11 +245,11 @@ namespace Baza
 
         // Zapytanie zwraca nazwiska lekarzy którzy kiedykolwiek pełnią dyżur w wybranej przychodni
                 var query_lista_lekarzy =
-                from l in db.Lekarze
-                join dyz in db.Dyzury on l.IDLekarz equals dyz.IDLekarz
-                join przych in db.Przychodnie on dyz.IDPrzychodnia equals przych.IDPrzychodnia
-                where przych.Rodzaj == nazwa_przychodni
-                select l.Nazwisko;
+                    from l in db.Lekarze
+                    join dyz in db.Dyzury on l.IDLekarz equals dyz.IDLekarz
+                    join przych in db.Przychodnie on dyz.IDPrzychodnia equals przych.IDPrzychodnia
+                    where przych.Rodzaj == nazwa_przychodni
+                    select l.Nazwisko;
       
                     lekarzeComboBox.Items.Clear();
 
@@ -278,7 +283,6 @@ namespace Baza
                 textBoxPacjentID.Text = "";
             }
         }
-
        private void lekarzeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             // Błąd polegał na nakazaniu mu pracy na nullu, teraz instrukcja wykonuje się dopiero po sprawdzeniu że na comboboxie wybraliśmy jakiegoś lekarza
@@ -290,16 +294,21 @@ namespace Baza
                 ComboBox comboBox = (ComboBox)sender;
                 string tmp = comboBox.SelectedValue.ToString();
                 string[] words = tmp.Split(' ');
-                string nazwisko_lekarza = words[1];
+                nazwisko_lekarza = words[1];
                 var tmpID = from l in db.Lekarze where l.Nazwisko == nazwisko_lekarza select l.IDLekarz;
-                int id = 0;
-                foreach (var i in tmpID){id = i;}
-                tuBedzieNazwisko.Content = nazwisko_lekarza + " " + "Id: " + id ; 
+                foreach (var i in tmpID){ idWybranegoLekarza = i;}
+                tuBedzieNazwisko.Content = nazwisko_lekarza; 
                 //siatka tygodniowa ma wypełnic się dyżurami wybranego lekarza w biezącym tygodniu
                 
  //zapisuje dane o godzinach dyzurow lekarza do 2 list
-                var odgodziny = from d in db.Dyzury where d.IDLekarz == id orderby d.DzienTygodnia select d.OdGodziny;
-                var dogodziny = from d in db.Dyzury where d.IDLekarz == id orderby d.DzienTygodnia select d.DoGodziny;
+                var dniDyzurow = from d in db.Dyzury where d.IDLekarz == idWybranegoLekarza orderby d.DzienTygodnia select d.DzienTygodnia;
+                var odgodziny = from d in db.Dyzury where d.IDLekarz == idWybranegoLekarza orderby d.DzienTygodnia select d.OdGodziny;
+                var dogodziny = from d in db.Dyzury where d.IDLekarz == idWybranegoLekarza orderby d.DzienTygodnia select d.DoGodziny;
+                List<int> dniPracy = new List<int>();
+                foreach(var item in dniDyzurow)
+                {
+                   dniPracy.Add(item);
+                }
                 List<int> start = new List<int>();
                 foreach (var item in odgodziny)
                 {
@@ -311,13 +320,12 @@ namespace Baza
                    finish.Add(item);
                 }
                 int idx = 0;
-                foreach(int s  in start )
+                foreach(int s  in dniPracy )
                 {
-                    int ilegodzin = finish[idx] - s;
-                   // textBoxPacjentID.Text += ilegodzin.ToString() + "-";
-                    idx++;
+                    int ilegodzin = finish[idx] - s;                  
+ 
                 //odblokowuje tyle przycisków ile godzin przyjmuje lekarz 
-                    if (idx == 1)
+                    if (s == 1)
                     {
                         int licznik = 0;
                         foreach (UIElement b in _PN_.Children)
@@ -328,7 +336,7 @@ namespace Baza
                                 {
                                     string[] dyzurGodzina = ((Button)b).Content.ToString().Split(':');
                                     int g = Convert.ToInt32(dyzurGodzina[0]);
-                                    if (g >= start[0] && g<= finish[0])
+                                    if (g >= start[idx] && g<= finish[idx])
                                     {
                                    ((Button)b).IsEnabled = true;
                                     ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
@@ -337,8 +345,10 @@ namespace Baza
                                 }
                             }
                         }
+                        idx++;
+                        continue;
                     }
-                    if (idx == 2)
+                 else   if (s == 2)
                     {
                         int licznik = 0;
                         foreach (UIElement b in _WT_.Children)
@@ -349,7 +359,7 @@ namespace Baza
                                 {
                                     string[] dyzurGodzina = ((Button)b).Content.ToString().Split(':');
                                     int g = Convert.ToInt32(dyzurGodzina[0]);
-                                    if (g >= start[1] && g <= finish[1])
+                                    if (g >= start[idx] && g <= finish[idx])
                                     {
                                         ((Button)b).IsEnabled = true;
                                         ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
@@ -358,8 +368,10 @@ namespace Baza
                                 }
                             }
                         }
+                        idx++;
+                        continue;
                     }
-                    if (idx == 3)
+                  else  if (s == 3)
                     {
                         int licznik = 0;
                         foreach (UIElement b in _SR_.Children)
@@ -370,7 +382,7 @@ namespace Baza
                                 {
                                     string[] dyzurGodzina = ((Button)b).Content.ToString().Split(':');
                                     int g = Convert.ToInt32(dyzurGodzina[0]);
-                                    if (g >= start[2] && g <= finish[2])
+                                    if (g >= start[idx] && g <= finish[idx])
                                     {
                                         ((Button)b).IsEnabled = true;
                                         ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
@@ -379,8 +391,10 @@ namespace Baza
                                 }
                             }
                         }
+                        idx++;
+                        continue;
                     }
-                    if (idx == 4)
+                  else  if (s == 4)
                     {
                         int licznik = 0;
                         foreach (UIElement b in _CZ_.Children)
@@ -391,7 +405,7 @@ namespace Baza
                                 {
                                     string[] dyzurGodzina = ((Button)b).Content.ToString().Split(':');
                                     int g = Convert.ToInt32(dyzurGodzina[0]);
-                                    if (g >= start[3] && g <= finish[3])
+                                    if (g >= start[idx] && g <= finish[idx])
                                     {
                                         ((Button)b).IsEnabled = true;
                                         ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
@@ -400,8 +414,10 @@ namespace Baza
                                 }
                             }
                         }
+                        idx++;
+                        continue;
                     }
-                    if (idx == 5)
+                 else   if (s == 5)
                     {
                         int licznik = 0;
                         foreach (UIElement b in _PT_.Children)
@@ -412,7 +428,7 @@ namespace Baza
                                 {
                                     string[] dyzurGodzina = ((Button)b).Content.ToString().Split(':');
                                     int g = Convert.ToInt32(dyzurGodzina[0]);
-                                    if (g >= start[4] && g <= finish[4])
+                                    if (g >= start[idx] && g <= finish[idx])
                                     {
                                         ((Button)b).IsEnabled = true;
                                         ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
@@ -421,16 +437,17 @@ namespace Baza
                                 }
                             }
                         }
+                        idx++;
+                        continue;
                     }
                 }
-                
             }          
         }
 
         private void calendarSelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             Project_context db = new Project_context();
-            string selectedDate = calendar.SelectedDate.ToString();                     
+            selectedDate = calendar.SelectedDate.ToString();                     
             string[] dataTmp = selectedDate.Split(' ');
             string[] data = dataTmp[0].Split('.');         
             //string[] tydzien = { "poniedziałek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela" };
@@ -483,11 +500,9 @@ namespace Baza
             var textBox = sender as TextBox;
 
         }
-
         //inny kolor dla panelu admina
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            
+        {          
            if(adminTabHeader.IsSelected)
            {
                 SolidColorBrush brush = new SolidColorBrush(Colors.DarkOliveGreen);
@@ -497,8 +512,7 @@ namespace Baza
            {
               SolidColorBrush brush = new SolidColorBrush(Colors.BurlyWood);
               tabControl.Background = brush;
-           }   
-                
+           }                   
         }
        
         public void WyczyscSiatketygodniowa()
@@ -528,27 +542,61 @@ namespace Baza
   
         private void Click_dyzurButton(object sender, RoutedEventArgs e)
         {
-                UIElement b = sender as UIElement;
-                    if (b is Button )
+            //czyszczenie wczesniej zaznaczonych na pomaranczowo terminow
+            UIElementCollection[] dniSiatkitygodniowej = { _PN_.Children, _WT_.Children, _SR_.Children, _CZ_.Children, _PT_.Children };
+            foreach (var day in dniSiatkitygodniowej)
+            {
+                foreach (UIElement btn in day)
+                {
+                    if (btn is Button)
+                    {
+                        if (((Button)btn).Background.ToString() == Colors.Orange.ToString())
+                            ((Button)btn).Background = new SolidColorBrush(Colors.LimeGreen);
+                    }
+                }
+            }
+            //zamiana koloru terminu + godzina wizyty zapisywana do zmiennej
+            UIElement b = sender as UIElement;
+            if (b is Button)
             {
                 if (((Button)b).Background.ToString() == Colors.LimeGreen.ToString())
                 {
-                ((Button)b).Background = new SolidColorBrush(Colors.Orange);
-    //event zapisuje godzine wizyty do zmiennej
-                godzinaWizyty = ((Button)b).Content.ToString();  
+                    ((Button)b).Background = new SolidColorBrush(Colors.Orange);
+                    godzinaWizyty = ((Button)b).Content.ToString();
+
+                    //by znalezc ID dyzuru - musze znac wybrany dzien tygodnia                   
+                    foreach (var day in dniSiatkitygodniowej)
+                    {
+                        foreach (UIElement bt in day)
+                        {
+                            if (bt is Button)
+                            {
+                                if (((Button)bt).Background.ToString() == Colors.Orange.ToString())
+                                {
+                                    if (day == _PN_.Children)
+                                        numerDniaTygodnia = 1;
+                                    if (day == _WT_.Children)
+                                        numerDniaTygodnia = 2;
+                                    if (day == _SR_.Children)
+                                        numerDniaTygodnia = 3;
+                                    if (day == _CZ_.Children)
+                                        numerDniaTygodnia = 4;
+                                    if (day == _PT_.Children)
+                                        numerDniaTygodnia = 5;
+                                }
+                            }
+                        }
+                    }
                 }
-                    
                 else if (((Button)b).Background.ToString() == Colors.Orange.ToString())
                 {
-                ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
-                godzinaWizyty = "";
+                    ((Button)b).Background = new SolidColorBrush(Colors.LimeGreen);
+                    godzinaWizyty = "";
+                    numerDniaTygodnia = 0;
                 }
-
-            }                 
+            }
         }
-//teraz pora poskladac wszystko do jednego worku - do wizyty!
-//wizyta nie ma nazwiska lekarza - czy tak to ma być?
-       
+
         private void textBox_tylkoLiczby(object sender, TextCompositionEventArgs e)//tylko liczby
        {
            TextBox t = (TextBox)sender;
@@ -642,9 +690,17 @@ namespace Baza
             Project_context db = new Project_context();
             using (db)
             {
-                var wizyta = new Wizyta { IDPacjent = Int32.Parse(textBoxPacjentID.Text),
-        // Wyciągnięcie dyżuru na razie mi nie wychodzi: IDDyzur = 1, 
-                GodzinaWizyty = godzinaWizyty };
+                var _iddyzur = from d in db.Dyzury where d.DzienTygodnia == numerDniaTygodnia && d.IDLekarz == idWybranegoLekarza select d.IDDyzur;
+                foreach (var v in _iddyzur)
+                    iddyzur = v;
+                //podsumujmy: taka data bardziej poprawna by byla - 2016-06-23 23:40:51.840 ale mamy tam stringa, wiec to nieistotne
+                var wizyta = new Wizyta
+                {
+                    IDPacjent = Int32.Parse(textBoxPacjentID.Text),
+                    IDDyzur = iddyzur,
+                    GodzinaWizyty = godzinaWizyty,
+                    DataWizyty = selectedDate
+                };
                 db.Wizyty.Add(wizyta);
                 db.SaveChanges();
             }
